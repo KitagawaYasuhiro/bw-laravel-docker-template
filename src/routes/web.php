749128c9f8422980ebc445f1e2eed6787bc66201
yasuhiro kitagawa <yassun0827@gmail.com>
2025-09-25 03:12:57 +0900

    docs: スペルミスの修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,6 +16,6 @@ Route::get('/', function () {
 });
 
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'todoController@create')->name('todo.create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::post('/todo', 'TodoController@store')->name('todo.store');
 Route::get('/todo', 'TodoController@index')->name('todo.index');
